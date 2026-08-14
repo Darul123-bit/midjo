fdcfceea8048880f346dc8bb92781f8e5fcabd74
Darul123-bit <emmanuelbayaro09@gmail.com>
2026-08-14 15:03:58 +0000

    11th commit

--- main.py
+++ main.py
@@ -33,8 +33,9 @@ MAJORATION_TAXI = 0.25
 #   Variables
 PRIX_TOTAL = 0.0
 kilometrage = float()
-heure = datetime.time()
-
+heure = datetime.now()
+heurePointe = (datetime.time(7, 00) <= heure <= datetime.time(8, 45)) or (datetime.time(11, 45) <= heure <= datetime.time(13, 00)) or (datetime.time(17, 00) <= heure <= datetime.time(19, 00))
+print(heurePointe)
 
 #   Affichage de bienvenue à l'utilisateur
 print(60*'_')
@@ -59,12 +60,7 @@ try:
 
     while True:
         if choix in range(1, 3, 1):
-            match choix:
-                case 1:
-                    PRIX_TOTAL = PRIX_DE_BASE_ZEM
-                case 2:
-                    PRIX_TOTAL = PRIX_DE_BASE_TAXI
-
+            
             break           
         else:
             print(60*'_')
@@ -85,3 +81,22 @@ try:
         kilometrage = float(input("Quelle est la distance de vôtre trajet ? : "))        
 except:
         print("Veuillez entrer des informations valides")
+
+
+match choix:
+    case 1:
+        if heurePointe:
+            PRIX_TOTAL = (PRIX_DE_BASE_ZEM + (PRIX_ZEM_KM*kilometrage))
+            PRIX_TOTAL *= MAJORATION_ZEM
+        else:
+            PRIX_TOTAL = (PRIX_DE_BASE_ZEM + (PRIX_ZEM_KM*kilometrage))
+    case 2:
+        if heurePointe:
+            PRIX_TOTAL = (PRIX_DE_BASE_TAXI + (PRIX_TAXI_KM*kilometrage))
+            PRIX_TOTAL *= MAJORATION_TAXI
+        else:
+            PRIX_TOTAL = (PRIX_DE_BASE_TAXI + (PRIX_TAXI_KM*kilometrage))
+
+print(60*'_')
+print(f"Vôtre trajet vous reviens à {PRIX_TOTAL}".center(60, ' '))
+print(60*'_')
