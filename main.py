c21e8109e6a450dee4f60c765fd918c6691eb12e
Darul123-bit <emmanuelbayaro09@gmail.com>
2026-08-14 14:14:21 +0000

    9th commit

--- main.py
+++ main.py
@@ -31,27 +31,50 @@ PRIX_TAXI_KM = 100
 MAJORATION_TAXI = 0.25
 
 #   Variables
+PRIX_TOTAL = 0.0
 kilometrage = float()
-heure = time()
-print(heure)
+heure = datetime.time()
+
 
 #   Affichage de bienvenue à l'utilisateur
-
+print(60*'_')
+print(f'heure: {heure}')
 print(60*'_')
 print(" Bienvenue sur Midjo ".center(60,' '))
 print(60*'_')
 print("\nCet programme écrit en Python vous permet de déterminer le \ncoût de toutes vos courses à Lomé.")
+print(60*'_')
+
 
 
 #   Collecte d'information sur le trajet
 
 try:
-    print("Quelle moyen utiliserez-vous ?".center(60, ' '))
+    print("Quelle moyen de transport utiliserez-vous ?".center(60, ' '))
+    print()
     print("\t1 • Zemidjan (Taxi-moto)")
     print("\t2 • Taxi (Voiture)")
     print(60*'_')
+    choix = input("\tTapez 1 ou 2 : ")
 
-    kilometrage = float(input("Quelle est la distance de vôtre trajet ? : ".center(60, ' ')))
+    while True:
+        if choix in range(1, 3, 1):
+            match choix:
+                case 1:
+                    PRIX_TOTAL = PRIX_DE_BASE_ZEM
+                    break
+                case 2:
+                    PRIX_TOTAL = PRIX_DE_BASE_TAXI
+                    break
+        else:
+            print(60*'_')
+            print("\t1 • Zemidjan (Taxi-moto)")
+            print("\t2 • Taxi (Voiture)")
+            print(60*'_')
+            choix = input("\tTapez 1 ou 2 : ")
+            
+    print(60*'_')
+    kilometrage = float(input("\tQuelle est la distance de vôtre trajet ? : "))
     if kilometrage > 0.0:
         pass
     elif kilometrage == 0.0:
